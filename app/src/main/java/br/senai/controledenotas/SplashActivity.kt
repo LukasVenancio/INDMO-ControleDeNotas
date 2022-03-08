ea87416c65a2fd002d74ca6f8e0a6ff149bfc242
LUKAS SANTOS VENANCIO <21276375@cfp127aluno.local>
2022-03-08 14:27:23 -0300

    Splash activity created.

---
+++ app/src/main/java/br/senai/controledenotas/SplashActivity.kt
@@ -0,0 +1,21 @@
+package br.senai.controledenotas
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.*
+import kotlin.concurrent.schedule
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Timer().schedule(2000){iniciarMain()}
+    }
+    private fun iniciarMain(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
